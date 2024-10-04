b15954ca02b6c59b50be7952166df40ccc4bb5cb
rouve <rouvemartin@gmail.com>
2024-10-04 08:05:01 +0200

    init

--- app/Models/Batch.php
+++ app/Models/Batch.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Batch extends Model
 {
     use HasFactory;
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'company_name'];
 
     public function items(): HasMany
     {
